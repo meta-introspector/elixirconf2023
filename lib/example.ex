9a1ff568bd39dfaf0c385492b0fc3c6786e15833
mike dupont <mike.dupont@introspector.local>
2024-03-05 04:57:03 -0500

    mistral

--- lib/example.ex
+++ lib/example.ex
@@ -1,39 +1,32 @@
 defmodule Example do
   @moduledoc false
 
-  def tune() do
-    Example.Tune.train()
-  end
-
-  def evaluate() do
-    Example.Evaluate.evaluate()
-  end
-
-  def cancellations() do
-    Example.Evaluate.cancellations()
-  end
-
-  def get_token_ids() do
-    # [101, 1996, 3976, 2001, 25506, 102]
-    text = "the price was outrageous"
-    {:ok, tokenizer} = Tokenizers.Tokenizer.from_pretrained("google-bert/bert-base-uncased")
-    {:ok, encoding} = Tokenizers.Tokenizer.encode(tokenizer, text)
-    Tokenizers.Encoding.get_tokens(encoding) |> IO.inspect(label: "tokens")
-    Tokenizers.Encoding.get_ids(encoding) |> IO.inspect(label: "token ids")
-  end
-
-  def embeddings(token_ids) when is_list(token_ids) do
-    {:ok, %{model: model, params: params}} =
-      Bumblebee.load_model({:hf, "bert-base-cased"},
-        architecture: :base
+  def test() do
+    repo = {:hf, "mistralai/Mistral-7B-Instruct-v0.2"}
+    
+    {:ok, model_info} = Bumblebee.load_model(repo, type: :bf16, backend: EXLA.Backend)
+    {:ok, tokenizer} = Bumblebee.load_tokenizer(repo)
+    {:ok, generation_config} = Bumblebee.load_generation_config(repo)
+    
+    generation_config =
+      Bumblebee.configure(generation_config,
+	max_new_tokens: 256,
+	strategy: %{type: :multinomial_sampling, top_p: 0.6}
       )
 
-    inputs = %{
-      "input_ids" => Nx.tensor([token_ids])
-    }
+    serving =
+      Bumblebee.Text.generation(model_info, tokenizer, generation_config,
+	compile: [batch_size: 1, sequence_length: 1028],
+	stream: true,
+	defn_options: [compiler: EXLA]
+      )
+    
+    # Should be supervised
 
-    outputs = Axon.predict(model, params, inputs)
 
-    outputs.hidden_state
+    text = "Hello how are you?"
+    Nx.Serving.run(serving, text)
+#    Nx.Serving.batched_run(Mistral, prompt) |> Enum.each(&IO.write/1)    
   end
+
 end
--- mix.exs
+++ mix.exs
@@ -22,11 +22,11 @@ defmodule Example.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_csv, "~> 1.2"},
-      {:csv, "~> 2.4"},
-      {:bumblebee, "~> 0.3.1"},
-      {:exla, "~> 0.6"},
-      {:nx, "~> 0.6"}
+      {:nimble_csv, ">= 1.2.0"},
+      {:csv, ">= 2.4.0"},
+      {:bumblebee, ">= 0.5.3"},
+      {:exla, ">= 0.6.0"},
+      {:nx, ">= 0.6.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
